61f76c2674b144f45b73907eb26d1357f447f4bf
Shahab <Shahab@Shahabs-MacBook-Pro.local>
2020-01-23 20:23:13 +0530

    Updates

---
+++ precache-manifest.2ca09be996d9cdd33598112794d14d39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad51e774283f576994a45272edba865",
+    "url": "/cart-app/index.html"
+  },
+  {
+    "revision": "caed9e70e2f8f137f8a9",
+    "url": "/cart-app/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "7a5e37bacf4d02721ed7",
+    "url": "/cart-app/static/css/main.2909adde.chunk.css"
+  },
+  {
+    "revision": "caed9e70e2f8f137f8a9",
+    "url": "/cart-app/static/js/2.1ab74ef7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/cart-app/static/js/2.1ab74ef7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a5e37bacf4d02721ed7",
+    "url": "/cart-app/static/js/main.af43caac.chunk.js"
+  },
+  {
+    "revision": "a3cec9bd17bf9a8e6f92",
+    "url": "/cart-app/static/js/runtime-main.99499525.js"
+  }
+]);
